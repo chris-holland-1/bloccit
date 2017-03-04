e261db9bd7f77eb72cde8558bbaa5b3adf92ab46
Chris Holland <chris.holland987@gmail.com>
2017-03-03 17:06:23 -0800

    Checkpoint 27 Posts: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,16 @@
 
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
  15.times do
    Topic.create!(
@@ -20,9 +30,10 @@ require 'random_data'
 # Create Posts
 50.times do
   Post.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -35,7 +46,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'chris.holland987.com',
+  password: 'helloworld' 
+ )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
